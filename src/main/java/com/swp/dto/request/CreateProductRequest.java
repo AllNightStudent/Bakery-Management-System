a51cc62a80eaaa741324094eb7d443c4d3b152f9
namntp27 <namntp27@gmail.com>
2025-11-06 00:54:12 +0700

    validate product

--- src/main/java/com/swp/dto/request/CreateProductRequest.java
+++ src/main/java/com/swp/dto/request/CreateProductRequest.java
@@ -3,6 +3,7 @@ package com.swp.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,10 +14,13 @@ import java.time.LocalDate;
 public class CreateProductRequest {
 
     @NotBlank
+    @Size(min = 2, max = 100, message = "Tên sản phẩm phải từ 2-100 ký tự")
     private String name;
 
+    @Size(max = 255, message = "Mô tả ngắn không được vượt quá 255 ký tự")
     private String shortDescription;
 
+    @Size(max = 2000, message = "Mô tả chi tiết không được vượt quá 2000 ký tự")
     private String description;
 
     @NotNull
